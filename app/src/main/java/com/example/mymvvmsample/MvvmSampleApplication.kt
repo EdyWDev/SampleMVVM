762fe45788296f35b24bc4752bae212b2ba100b6
EdyWDev <edyta.wysocka.1994@gmail.com>
2022-09-03 11:25:07 +0200

    Add Hilt to project

--- app/src/main/java/com/example/mymvvmsample/MvvmSampleApplication.kt
+++ app/src/main/java/com/example/mymvvmsample/MvvmSampleApplication.kt
@@ -1,4 +1,8 @@
 package com.example.mymvvmsample
 
-class MvvmSampleApplication {
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MvvmSampleApplication : Application() {
 }
